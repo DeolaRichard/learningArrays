fc191327fb034cd88370b57dde751fe051063622
DeolaRichard <adekunlerichie5@gmail.com>
2023-07-31 04:26:34 +0100

    wrote code to sort the array in ascending order and to also find the factorial of a given number

--- arrays.py
+++ arrays.py
@@ -1,6 +1,10 @@
 from array import *
 
 vals = array("i", [5, 9, 8, 4, 2, 9, 3, 0])
+# to sort the array in ascending order without modifying the initial array
+sorted_vals = sorted(vals)
+print("Sorted array: ", sorted_vals)
+
 # getting the address and length of the array
 print(vals.buffer_info())
 
@@ -11,3 +15,22 @@ vals.reverse()
 for i in range(len(vals)):
     print(vals[i])
 print(newArr)
+
+
+# write a code to find a factorial of a given number
+def factorial(n):
+    if n < 0:
+        return "Factorial not available for negative numbers"
+    elif n == 0 or n == 1:
+        return 1
+    else:
+        result = 1
+        for b in range(2, n + 1):
+            result *= b
+        return result
+
+
+# Test the function with some examples
+print(factorial(10))
+print(factorial(3))
+
